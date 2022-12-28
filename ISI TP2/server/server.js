67039ee41dbe5bd9c981f8f99cc8e51a35a239f9
André Freitas <andregabriel@live.com.pt>
2022-12-28 16:24:40 +0000

    Integrar Swagger. Documentar /api/livros.

--- ISI TP2/server/server.js
+++ ISI TP2/server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const apiRouter = require('./routes');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
 const app = express();
 
 app.use(express.json());
 
 app.use('/api/livros', apiRouter);
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 app.listen(process.env.PORT || '3000', () => {
 
